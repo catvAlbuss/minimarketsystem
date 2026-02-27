ae43059aa05e8fc774f9e216783c56cf5fa4731b
Jackortega922 <jackortega922@gmail.com>
2026-02-27 12:32:24 -0500

    limpiar codigo apsidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-<<<<<<< HEAD
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
-=======
+
 import { BookOpen, Folder, LayoutGrid, User } from 'lucide-vue-next';
->>>>>>> ca91998998c9ef5e8dea0e0e1d114c3b708910ba
+
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
@@ -20,20 +18,18 @@ import {
 import { type NavItem } from '@/types';
 import AppLogo from './AppLogo.vue';
 import { dashboard } from '@/routes';
-<<<<<<< HEAD
+
 // IMPORTAR RUTAS
 import products from '@/routes/products';
 import users from '@/routes/users';
 import customers from '@/routes/customers';
 import categories from '@/routes/categories';
 import sales from '@/routes/sales';
-=======
-import products from '@/routes/products';
-import users from '@/routes/users';
-import customers from '@/routes/customers';
+
+
 import providers from '@/routes/providers';
 import promotions from '@/routes/promotions';
->>>>>>> ca91998998c9ef5e8dea0e0e1d114c3b708910ba
+
 
 const mainNavItems: NavItem[] = [
     {
@@ -47,16 +43,11 @@ const mainNavItems: NavItem[] = [
         icon: LayoutGrid,
     },
     {
-<<<<<<< HEAD
         title: 'Productos',
-=======
-        title: 'Products',
->>>>>>> ca91998998c9ef5e8dea0e0e1d114c3b708910ba
         href: products.index.url(),
         icon: LayoutGrid,
     },
     {
-<<<<<<< HEAD
         title: 'Ventas',
         href: sales.index.url(),
         icon: LayoutGrid,
@@ -70,22 +61,18 @@ const mainNavItems: NavItem[] = [
         title: 'Clientes',
         href: customers.index.url(),
         icon: LayoutGrid,
-=======
-        title: 'Clientes',
-        href: customers.index.url(),
-        icon: LayoutGrid,
     },
     {
         title: 'Proveedores',
         href: providers.index.url(),
-        icon: User,
+        icon: LayoutGrid,
     },
     {
         title: 'Promociones',
         href: promotions.index.url(),
         icon: LayoutGrid,
->>>>>>> ca91998998c9ef5e8dea0e0e1d114c3b708910ba
-    }
+
+    },
 ];
 </script>
 
